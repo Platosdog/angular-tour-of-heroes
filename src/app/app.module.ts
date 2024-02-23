4b620869b6481399478b7dd7c4323df6ff1145ba
Platosdog <62915572+Platosdog@users.noreply.github.com>
2024-02-23 09:41:47 +0900

    its working

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,25 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
-import { HeroesComponent } from './heroes/heroes.component';
-import { HeroDetailComponent } from './hero-detail/hero-detail.component';
-import { MessagesComponent } from './messages/messages.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { HeroesComponent } from './heroes/heroes.component';
+import { MessagesComponent } from './messages/messages.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeroesComponent,
-    MessagesComponent,
-    DashboardComponent
-  ],
   imports: [
     BrowserModule,
     FormsModule,
-    HeroDetailComponent,
+    AppRoutingModule,
+    HeroDetailComponent
   ],
-  providers: [
-
+  declarations: [
+    AppComponent,
+    DashboardComponent,
+    HeroesComponent,
+    MessagesComponent
   ],
   bootstrap: [ AppComponent ]
 })
